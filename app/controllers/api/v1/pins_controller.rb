13c96c9206d1f39bd69acfdab0e3d8089aed087c
roberto <rueda.roberto89@gmail.com>
2017-08-10 11:15:49 -0500

    moved the token to api controller

--- app/controllers/api/v1/pins_controller.rb
+++ app/controllers/api/v1/pins_controller.rb
@@ -1,5 +1,14 @@
 class Api::V1::PinsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate
+  
+  def authenticate
+    user = User.find_by(email: request.headers['HTTP_X_USER_EMAIL'])
+    access = user ? request.headers['X-API-TOKEN'] == user.api_token : false
+    unless access
+    render json: { errors: "Acceso denegado" }, status: 401
+    end
+  end
+
 
   def index
     render json: Pin.all.order('created_at DESC')
